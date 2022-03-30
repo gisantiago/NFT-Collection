a8eac64976eec04cde8666509daadcb6efd5b5e2
gisantiago <gisantiago@live.com>
2022-03-30 15:32:22 -0400

    contract deplyment

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,17 +1,17 @@
-const { ethers } = require('hardhat');
-require('dotenv').config({ path: '.env' });
-const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require('../constants');
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
-const main = async () => {
+async function main() {
   // Address of the whitelist contract that you deployed in the previous module
   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
-  // URL from where we can extract the metadata for a Crypto dev NFT
+  // URL from where we can extract the metadata for a Crypto Dev NFT
   const metadataURL = METADATA_URL;
   /*
-    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-    so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
-    */
-  const cryptoDevsContract = await ethers.getContractFactory('CryptoDevs');
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
+  */
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
 
   // deploy the contract
   const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
@@ -20,13 +20,16 @@ const main = async () => {
   );
 
   // print the address of the deployed contract
-  console.log('Crypto devs Contract Address', deployedCryptoDevsContract);
-};
+  console.log(
+    "Crypto Devs Contract Address:",
+    deployedCryptoDevsContract.address
+  );
+}
 
-// Call the main func and catch if there is any error
+// Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   });
